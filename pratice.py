3c2576318aae2596cc8868ffbeebf9733b964eba
Richa Mahajan <richamahajan1617@gmail.com>
2023-12-06 09:17:22 +0100

    List and dictnaries in python

--- pratice.py
+++ pratice.py
@@ -1,1 +1,18 @@
-print("hello, wolrd")
+print("hello, world")
+
+#In python we have list which acts like array
+array= [1,2,4,5,7,8,9]
+
+for num in array:
+    print(num)
+
+#Objects are called dictonaries
+person={
+    "fname" :"richa",
+    "age":25,
+    "hobbies": ["dance", "play"]
+}
+
+for key in person:
+    print(key)
+    print (person[key])
