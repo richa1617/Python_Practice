bf159730b166c32cf3110233382e41d19194ee37
Richa Mahajan <richamahajan1617@gmail.com>
2023-12-06 10:34:58 +0100

    Learned dic methods in python

--- dictionary.py
+++ dictionary.py
@@ -7,32 +7,32 @@ student = {
     "hobbies": ["Jogging", "Gym", "Boxing"]
 }
 
-# Access an entry
-#print(student["age"])
-# print(student.name) doesnt work
-
-mixed_dictionary = {
-    (0, 0): "Origin",
-    1000: "A thousand",
-    3.141: "Pi",
-    True: "This is stupid",
-    print: "A function for logging to console",
-    "Why": "I dont know"
-}
-
-"""for keys in mixed_dictionary:
-    print(keys)"""
-"""
-print(True in mixed_dictionary)    
-print("Why" in mixed_dictionary)  
-print("X" in mixed_dictionary)  
-"""
 
 
-# Printing the items of the dictionary
-items = mixed_dictionary.items()
-print(items)
 
-# Printing the keys of the dictionary
-keys = mixed_dictionary.keys()
-print(keys)
+student["name"] ="Richa"
+print(student)
+#print(student["school"])
+print(student.get("school"))
+print(student.keys()) # dict_keys(['name', 'graduated', 'married', 'age', 'hobbies'])
+#print (student.items())
+#dict_items([('name', 'Alice'), ('graduated', False), ('married', False), ('age', 23), ('hobbies', ['Jogging', 'Gym', 'Boxing'])])
+
+print(len(student))
+
+person =["richa", "akash", "aman", "su"]
+
+dic= dict.fromkeys(person)
+print(dic) #{'richa': None, 'akash': None, 'aman': None, 'su': None}
+
+dic2= dict.fromkeys(person,1)
+print(dic2) #{'richa': 1, 'akash': 1, 'aman': 1, 'su': 1}
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8]
+
+numDic={}
+
+for num in nums:
+    numDic[num]=str(num)
+
+print(numDic)
